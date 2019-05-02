1c7fbc6289dfc5ae42919a63f2ea269a09452e36
naronA <otherthan.new@gmail.com>
2019-05-02 17:59:53 +0900

    mat64ではなくmatrixにする

--- mat/mat.go
+++ mat/mat.go
@@ -9,20 +9,20 @@ import (
 	"github.com/naronA/zero_deeplearning/scalar"
 )
 
-type Mat64 struct {
+type Matrix struct {
 	Array   array.Array
 	Rows    int
 	Columns int
 }
 
-func (m *Mat64) Shape() (int, int) {
+func (m *Matrix) Shape() (int, int) {
 	if m.Rows == 1 {
 		return m.Columns, 0
 	}
 	return m.Rows, m.Columns
 }
 
-func (m *Mat64) Element(r int, c int) float64 {
+func (m *Matrix) Element(r int, c int) float64 {
 	index := r*m.Columns + c
 	if len(m.Array) <= index {
 		log.Println(len(m.Array), index)
@@ -31,7 +31,7 @@ func (m *Mat64) Element(r int, c int) float64 {
 	return m.Array[index]
 }
 
-func (m *Mat64) SliceRow(r int) array.Array {
+func (m *Matrix) SliceRow(r int) array.Array {
 	slice := make(array.Array, m.Columns)
 	for i := 0; i < len(slice); i++ {
 		slice[i] = m.Array[i+r*m.Columns]
@@ -39,7 +39,7 @@ func (m *Mat64) SliceRow(r int) array.Array {
 	return slice
 }
 
-func (m *Mat64) String() string {
+func (m *Matrix) String() string {
 	str := "[\n"
 	for i := 0; i < m.Rows; i++ {
 		str += fmt.Sprintf("  %v,\n", m.SliceRow(i))
@@ -48,7 +48,7 @@ func (m *Mat64) String() string {
 	return str
 }
 
-func Zeros(rows int, cols int) *Mat64 {
+func Zeros(rows int, cols int) *Matrix {
 	zeros := make(array.Array, rows*cols)
 	for i := range zeros {
 		zeros[i] = 0
@@ -60,7 +60,7 @@ func Zeros(rows int, cols int) *Mat64 {
 	return mat
 }
 
-func ZerosLike(x *Mat64) *Mat64 {
+func ZerosLike(x *Matrix) *Matrix {
 	zeros := make(array.Array, x.Rows*x.Columns)
 	for i := range zeros {
 		zeros[i] = 0
@@ -72,30 +72,30 @@ func ZerosLike(x *Mat64) *Mat64 {
 	return mat
 }
 
-func NewMat64(row int, column int, array array.Array) (*Mat64, error) {
+func NewMat64(row int, column int, array array.Array) (*Matrix, error) {
 	if row == 0 || column == 0 {
 		return nil, errors.New("row/columns is zero.")
 	}
-	return &Mat64{
+	return &Matrix{
 		Array:   array,
 		Rows:    row,
 		Columns: column,
 	}, nil
 }
 
-func NewRandnMat64(row int, column int) (*Mat64, error) {
+func NewRandnMat64(row int, column int) (*Matrix, error) {
 	if row == 0 || column == 0 {
 		return nil, errors.New("row/columns is zero.")
 	}
 	array := array.Randn(row * column)
-	return &Mat64{
+	return &Matrix{
 		Array:   array,
 		Rows:    row,
 		Columns: column,
 	}, nil
 }
 
-func (m1 *Mat64) NotEqual(m2 *Mat64) bool {
+func (m1 *Matrix) NotEqual(m2 *Matrix) bool {
 	if m1.Rows == m2.Rows &&
 		m1.Columns == m2.Columns &&
 		m1.Array.Equal(m2.Array) {
@@ -104,7 +104,7 @@ func (m1 *Mat64) NotEqual(m2 *Mat64) bool {
 	return true
 }
 
-func (m1 *Mat64) Equal(m2 *Mat64) bool {
+func (m1 *Matrix) Equal(m2 *Matrix) bool {
 	if m1.Rows == m2.Rows &&
 		m1.Columns == m2.Columns &&
 		m1.Array.Equal(m2.Array) {
@@ -113,7 +113,7 @@ func (m1 *Mat64) Equal(m2 *Mat64) bool {
 	return false
 }
 
-func (m1 *Mat64) Dot(m2 *Mat64) *Mat64 {
+func (m1 *Matrix) Dot(m2 *Matrix) *Matrix {
 	if m1.Columns != m2.Rows {
 		return nil
 	}
@@ -126,23 +126,23 @@ func (m1 *Mat64) Dot(m2 *Mat64) *Mat64 {
 			}
 		}
 	}
-	return &Mat64{
+	return &Matrix{
 		Array:   mat,
 		Rows:    m1.Rows,
 		Columns: m2.Columns,
 	}
 }
 
-func (m1 *Mat64) Mul(m2 *Mat64) *Mat64 {
+func (m1 *Matrix) Mul(m2 *Matrix) *Matrix {
 	mul := m1.Array.Multi(m2.Array)
-	return &Mat64{
+	return &Matrix{
 		Array:   mul,
 		Rows:    m1.Rows,
 		Columns: m1.Columns,
 	}
 }
 
-func (m1 *Mat64) Add(m2 *Mat64) *Mat64 {
+func (m1 *Matrix) Add(m2 *Matrix) *Matrix {
 	// 左辺の行数と、右辺の列数があっていないの掛け算できない
 	if m1.Columns != m2.Columns && m1.Rows != m2.Rows {
 		return nil
@@ -155,14 +155,14 @@ func (m1 *Mat64) Add(m2 *Mat64) *Mat64 {
 			mat[index] = m1.Element(r, c) + m2.Element(r, c)
 		}
 	}
-	return &Mat64{
+	return &Matrix{
 		Array:   mat,
 		Rows:    m1.Rows,
 		Columns: m1.Columns,
 	}
 }
 
-func (m1 *Mat64) AddBroadCast(m2 *Mat64) *Mat64 {
+func (m1 *Matrix) AddBroadCast(m2 *Matrix) *Matrix {
 	// 左辺の行数と、右辺の列数があっていないの掛け算できない
 	if m1.Columns != m2.Columns {
 		return nil
@@ -175,13 +175,13 @@ func (m1 *Mat64) AddBroadCast(m2 *Mat64) *Mat64 {
 			mat[index] = m1.Element(r, c) + m2.Element(0, c)
 		}
 	}
-	return &Mat64{
+	return &Matrix{
 		Array:   mat,
 		Rows:    m1.Rows,
 		Columns: m1.Columns,
 	}
 }
-func (m *Mat64) AddAll(a float64) *Mat64 {
+func (m *Matrix) AddAll(a float64) *Matrix {
 
 	mat := make([]float64, m.Rows*m.Columns)
 	for r := 0; r < m.Rows; r++ {
@@ -190,14 +190,14 @@ func (m *Mat64) AddAll(a float64) *Mat64 {
 			mat[index] = m.Element(r, c) + a
 		}
 	}
-	return &Mat64{
+	return &Matrix{
 		Array:   mat,
 		Rows:    m.Rows,
 		Columns: m.Columns,
 	}
 }
 
-func (m *Mat64) MulAll(a float64) *Mat64 {
+func (m *Matrix) MulAll(a float64) *Matrix {
 	// 左辺の行数と、右辺の列数があっていないの掛け算できない
 	mat := make([]float64, m.Rows*m.Columns)
 	for r := 0; r < m.Rows; r++ {
@@ -206,14 +206,14 @@ func (m *Mat64) MulAll(a float64) *Mat64 {
 			mat[index] = a * m.Element(r, c)
 		}
 	}
-	return &Mat64{
+	return &Matrix{
 		Array:   mat,
 		Rows:    m.Rows,
 		Columns: m.Columns,
 	}
 }
 
-func Sigmoid(m *Mat64) *Mat64 {
+func Sigmoid(m *Matrix) *Matrix {
 	mat := make([]float64, m.Rows*m.Columns)
 	for r := 0; r < m.Rows; r++ {
 		for c := 0; c < m.Columns; c++ {
@@ -221,14 +221,14 @@ func Sigmoid(m *Mat64) *Mat64 {
 			mat[index] = scalar.Sigmoid(m.Element(r, c))
 		}
 	}
-	return &Mat64{
+	return &Matrix{
 		Array:   mat,
 		Rows:    m.Rows,
 		Columns: m.Columns,
 	}
 }
 
-func Relu(m *Mat64) *Mat64 {
+func Relu(m *Matrix) *Matrix {
 	mat := make([]float64, m.Rows*m.Columns)
 	for r := 0; r < m.Rows; r++ {
 		for c := 0; c < m.Columns; c++ {
@@ -236,27 +236,27 @@ func Relu(m *Mat64) *Mat64 {
 			mat[index] = scalar.Relu(m.Element(r, c))
 		}
 	}
-	return &Mat64{
+	return &Matrix{
 		Array:   mat,
 		Rows:    m.Rows,
 		Columns: m.Columns,
 	}
 }
 
-func Log(m *Mat64) *Mat64 {
+func Log(m *Matrix) *Matrix {
 	log := array.Log(m.Array)
-	return &Mat64{
+	return &Matrix{
 		Array:   log,
 		Rows:    m.Rows,
 		Columns: m.Columns,
 	}
 }
 
-func Sum(m *Mat64) float64 {
+func Sum(m *Matrix) float64 {
 	return array.Sum(m.Array)
 }
 
-func ArgMax(x *Mat64) []int {
+func ArgMax(x *Matrix) []int {
 	r := make([]int, x.Rows)
 	for i := 0; i < x.Rows; i++ {
 		row := x.SliceRow(i)
@@ -265,7 +265,7 @@ func ArgMax(x *Mat64) []int {
 	return r
 }
 
-func Softmax(x *Mat64) *Mat64 {
+func Softmax(x *Matrix) *Matrix {
 	m := array.Array{}
 	for i := 0; i < x.Rows; i++ {
 		xRow := x.SliceRow(i)
@@ -278,7 +278,7 @@ func Softmax(x *Mat64) *Mat64 {
 	return r
 }
 
-func CrossEntropyError(y, t *Mat64) float64 {
+func CrossEntropyError(y, t *Matrix) float64 {
 	r := make(array.Array, y.Rows)
 	for i := 0; i < y.Rows; i++ {
 		yRow := y.SliceRow(i)
@@ -288,7 +288,7 @@ func CrossEntropyError(y, t *Mat64) float64 {
 	return array.Sum(r) / float64(y.Rows)
 }
 
-func NumericalGradient(f func(array.Array) float64, x *Mat64) *Mat64 {
+func NumericalGradient(f func(array.Array) float64, x *Matrix) *Matrix {
 	grad := array.NumericalGradient(f, x.Array)
 	mat, _ := NewMat64(x.Rows, x.Columns, grad)
 	return mat
