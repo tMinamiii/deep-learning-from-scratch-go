7addb9fc6f37160ae3feece755010ba4f626746f
naronA <otherthan.new@gmail.com>
2019-05-05 13:43:07 +0900

    pythonコードはgit管理しない

--- python/main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-from common.functions import cross_entropy_error, sigmoid, softmax
-
-x = np.array([[3, 1, 0], [1, 4, 0]])
-y = np.array([[1, 0], [0, 1]])
-sm = softmax(x)
-cee = cross_entropy_error(y, y)
-print(cee)
